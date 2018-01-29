6542ffb7b86ec535d313c6208d07863a52831db0
Alex Pletzer <alexander@gokliya.net>
2018-01-29 21:01:06 +1300

    making the plot horizontal

--- specfem3d/004-specfem3d-speedup-ap.py
+++ specfem3d/004-specfem3d-speedup-ap.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from matplotlib import pylab
+import matplotlib.pylab as plt
 import numpy
+
+plt.figure()
 
 df = pd.DataFrame({'compiler-target': ['Gnu 4.9.3 x86-64', 'Gnu 4.9.3 broadwell', 
 	                                 'Gnu 7.1.0 broadwell', 'Gnu 7.1.0 x86-skylake', 
@@ -11,10 +13,10 @@ ts = numpy.array(df['times'].tolist())
 speedup = ts[0]/ts
 df['speedup'] = speedup
 
-p = df.plot(kind='bar', x='compiler-target', y='speedup', 
+df.plot(kind='barh', x='compiler-target', y='speedup', 
 	        width=0.5, 
 	        legend=False,
-	        color=[pylab.cm.Paired(numpy.arange(len(df)))],
-	        figsize=(3, 4) )
-pylab.title('speedup')
-pylab.show()
+	        color=['r', 'b', 'g', 'c', 'm', 'y',]) #pylab.cm.Paired(numpy.arange(len(df)))],)
+plt.axvline(1, color='k', linewidth=4)
+plt.title('speedup')
+plt.show()
